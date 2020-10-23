f4c354f88e6c389e15aa6ad59a9375801a2b0c39
Roland Womack <60858837+rolandstudios@users.noreply.github.com>
2020-10-23 17:35:42 -0400

    new bears

---
+++ Assets/Cloud.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Cloud : MonoBehaviour
+{
+
+    public SpriteRenderer sprite;
+    public Color col;
+    public Transform start;
+    public Transform end;
+    public float distAway;
+    public float thresh=.5f;
+    public float speed;
+    public float fadeThresh=3;
+    public Vector2 speedRange =new Vector2(.2f,.5f);
+   public Vector3 startPos;
+
+	private void Awake()
+	{
+		startPos=transform.position;
+        sprite=GetComponent<SpriteRenderer>();
+        col=Color.white;
+        col.a=.8f;
+        thresh=2;
+        fadeThresh=3;
+        speedRange = new Vector2(.1f, .25f);
+        speed =Random.Range(speedRange.x,speedRange.y);
+	}
+
+	private void OnEnable()
+	{
+		Restart();
+	}
+
+	void Restart()
+    {
+        transform.position = startPos;
+        col.a =.8f;
+        speed = Random.Range(speedRange.x, speedRange.y);
+    }
+    void Update()
+    {
+        transform.position += transform.right*Time.deltaTime*speed;
+        distAway=Vector3.Distance(end.position,transform.position);
+
+        if(distAway>fadeThresh)
+        {
+            col.a += Time.deltaTime;
+
+        }
+        else
+        {
+            col.a-=Time.deltaTime;
+        }
+        col.a = Mathf.Clamp(col.a,0,.6f);
+        sprite.color = col;
+
+        if (distAway<thresh)
+        {
+            transform.position = new Vector3(start.position.x, startPos.y, startPos.z);
+            col.a=0;
+        }
+        
+     
+    }
+}
--- Assets/Scripts/Bear.cs
+++ Assets/Scripts/Bear.cs
@@ -9,26 +9,47 @@ public class Bear : MonoBehaviour
     public Transform endPos;
 	public float lerpSpeed=1;
 	public Animator anim;
-	public float animSpeed=.25f;
+	public float animSpeed;
 	[Range(0,1)] public float lerp;
 
 	public enum Type {Linear, Float };
 	public Type type;
 
 	int idleId = Animator.StringToHash("Base Layer.BearIdle");
-	int walkId = Animator.StringToHash("Base Layer.BearWalk");
+	int walkId = Animator.StringToHash("Base Layer.BearWalkNew");
 
-	Vector3 additiveFloatVector;
-	[ShowIf("type", Type.Float)] public Vector3 additiveFloatVectorDir;
+	[ShowIf("type", Type.Float)] public float sinPhase;
+	[ShowIf("type", Type.Float)] public float sinAmp;
+	[ShowIf("type", Type.Float)] public float sinSpeed; 
+	[ShowIf("type", Type.Float)] public Vector3 additiveFloatVector;
 
+	public Vector3 startScale;
+
+
+	private void Awake()
+	{
+		if(anim)
+		{
+		startScale = transform.localScale;
+		transform.localScale = startScale * .7f;
+		anim.SetBool("Idle", false);
+		anim.Play(walkId);
+		anim.speed=Random.Range(.24f,.4f);
+		}
+	}
 	public void Restart()
 	{
 		lerp=0;
 		if(anim)
 		{
-			anim.speed= animSpeed;
-			anim.Play(walkId);
-			additiveFloatVector=Vector3.zero;
+			
+			
+			anim.Play(walkId,0);
+			sinPhase=0;
+			additiveFloatVector= Vector3.zero;
+			sinPhase=0;
+			transform.localScale = startScale*.7f;
+			anim.speed = Random.Range(.25f, .4f);
 		}
 	}
 
@@ -36,16 +57,25 @@ public class Bear : MonoBehaviour
 	{
 		lerp +=Time.deltaTime*lerpSpeed;
 		lerp = Mathf.Clamp01(lerp);
+
 		switch(type)
 		{
-			case Type.Linear: transform.position = Vector3.Lerp(startPos.position, endPos.position, lerp); break;
+			case Type.Linear: 
+				transform.position = Vector3.Lerp(startPos.position, endPos.position, lerp); 
+				break;
+
 			case Type.Float:
-				transform.position = Vector3.Lerp(startPos.position, endPos.position, lerp)+ additiveFloatVector; 
-				additiveFloatVector += additiveFloatVectorDir *Time.deltaTime;
+				transform.position = Vector3.Lerp(startPos.position, endPos.position, lerp) + additiveFloatVector; 
+				sinPhase += Time.deltaTime*sinSpeed;
+				additiveFloatVector = new Vector3(Mathf.Sin(sinPhase)*sinAmp- (sinPhase * .5f), 0, sinPhase*.5f);
 				break;
 		}
 
 		if(lerp==1 && anim!=null)
+		{
+			animSpeed=1;
 			anim.Play(idleId);
+			transform.localScale=startScale;
+		}
 	}
 }
