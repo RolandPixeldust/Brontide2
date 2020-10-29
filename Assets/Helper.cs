5baa85177b6b9140446ceac6e6bd862ae08840cc
Roland Womack <roland@pixeldust.tv>
2020-10-29 12:32:24 -0400

    Helper Sync Added

--- Assets/Helper.cs
+++ Assets/Helper.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
-
+using UnityEditor.UI;
 
 public class Helper : MonoBehaviour
 {
-    public List<Loadable> myObjects;
+
+	[System.Serializable]
+	public struct LoadableAsset
+	{
+		public Loadable loadable;
+		public Sprite sprite;
+	}
+
+	public List<Loadable> myObjects;
+	public LoadableAsset [] myAssets;
+
+	
 
 	[Button]
 	public void GetRemaining()
@@ -30,6 +41,34 @@ public class Helper : MonoBehaviour
 
 			if(counted) myObjects.Add(objs[i]);
 
+		}
+	}
+
+	[Button(ButtonSizes.Large)]
+	public void GetAssets()
+	{
+
+		var objs = FindObjectsOfType<Loadable>();
+		myAssets = new LoadableAsset[objs.Length];
+		for (int i = 0; i < objs.Length; i++)
+		{
+			myAssets[i].loadable = objs[i];
+			Sprite s = null;
+			if (myAssets[i].loadable.image != null) s = myAssets[i].loadable.image.sprite;
+			if (myAssets[i].loadable.spriteRenderer != null) s = myAssets[i].loadable.spriteRenderer.sprite;
+
+			myAssets[i].sprite = s;
+
+		}
+	}
+
+	[Button(ButtonSizes.Large)]
+	public void ReAssignSprites()
+	{
+		for (int i = 0; i < myAssets.Length; i++)
+		{
+			if (myAssets[i].loadable.image != null) myAssets[i].loadable.image.sprite = myAssets[i].sprite;
+			if (myAssets[i].loadable.spriteRenderer != null) myAssets[i].loadable.spriteRenderer.sprite = myAssets[i].sprite;
 		}
 	}
 
